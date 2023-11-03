c2e60d89dce6deab28a046d11dea25a43982e7df
Armando Guimarães Junior <armando.rogerio.jr@gmail.com>
2023-11-03 17:21:35 -0300

    Update SemanticAnalyzer.cs

--- SemanticAnalyzer.cs
+++ SemanticAnalyzer.cs
@@ -60,5 +60,85 @@ public class SemanticAnalyzer : RulesLangBaseVisitor<object>
         return context.GetText();
     }
 
+    // Método para visitar expressões aritméticas
+    public override object VisitArithmeticExpression([NotNull] RulesLangParser.ArithmeticExpressionContext context)
+    {
+        var left = Visit(context.term(0));
+        for (int i = 1; i <= context.term().Length / 2; i++)
+        {
+            var right = Visit(context.term(i));
+            var op = context.GetChild(i * 2 - 1).GetText();
+    
+            switch (op)
+            {
+                case "+":
+                    left = (int)left + (int)right;
+                    break;
+                case "-":
+                    left = (int)left - (int)right;
+                    break;
+                // Adicione casos para '*', '/' conforme necessário
+            }
+        }
+        return left;
+    }
+    
+    // Método para visitar termos (parte da expressão aritmética)
+    public override object VisitTerm([NotNull] RulesLangParser.TermContext context)
+    {
+        // Visita o primeiro fator (obrigatório)
+        var result = Visit(context.factor(0));
+    
+        // Se houver mais fatores, aplica as operações '*', '/'
+        for (int i = 1; i < context.factor().Length; i++)
+        {
+            var right = Visit(context.factor(i));
+            var op = context.GetChild((i - 1) * 2 + 1).GetText(); // Obtém o operador
+    
+            switch (op)
+            {
+                case "*":
+                    result = (int)result * (int)right;
+                    break;
+                case "/":
+                    if ((int)right == 0)
+                    {
+                        throw new DivideByZeroException("Cannot divide by zero.");
+                    }
+                    result = (int)result / (int)right;
+                    break;
+                default:
+                    throw new ArgumentException($"Unexpected operator '{op}'.");
+            }
+        }
+    
+        return result;
+    }
+
+    
+    // Método para visitar fatores (parte da expressão aritmética)
+    public override object VisitFactor([NotNull] RulesLangParser.FactorContext context)
+    {
+        // Se o fator é um número inteiro, retorne o valor
+        if (context.INT() != null)
+        {
+            return VisitInt(context.INT());
+        }
+        // Se o fator é um identificador, retorne o valor
+        else if (context.ID() != null)
+        {
+            return VisitId(context.ID());
+        }
+        // Se o fator é uma expressão entre parênteses, visite a expressão
+        else if (context.expression() != null)
+        {
+            return Visit(context.expression());
+        }
+        else
+        {
+            throw new ArgumentException("Invalid factor.");
+        }
+    }
+    
     // Adicionarei mais métodos de visita para outros tipos de expressões conforme necessário
 }
